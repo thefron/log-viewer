2dcf92cef66324b87856e32761fa708e3b23e8b6
Hoseong Hwang <thefron@wafflestudio.com>
2012-04-19 13:03:05 +0900

    Add build script

---
+++ public/javascripts/app.build.js
@@ -0,0 +1,14 @@
+({
+  baseUrl: '.',
+  paths: {
+    cs: 'lib/cs',
+    order: 'lib/order',
+    text: 'lib/text',
+    jquery: 'lib/jquery-1.7.2.min',
+    underscore: 'lib/underscore-min',
+    backbone: 'lib/backbone-min',
+    mustache: 'lib/mustache'
+  },
+  name: 'main',
+  out: 'main.production.js'
+})
